bd34fb06c95fc4064e987eb565874162df73af26
WebPatrickWorks <patrick.df68@email.com>
2025-07-16 11:38:03 -0300

    Ver 1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a487c15e1843a906d7d2bbee47dd0965
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1bd9fd1da46d94926cbcdaa5b205d037",
+"flutter_bootstrap.js": "a6d82c92709fb7829db7322bdd745cca",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
