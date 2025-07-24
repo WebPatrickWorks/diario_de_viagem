ca3242ee413a1bde4cb34060c58e8aadcd1d9a36
WebPatrickWorks <patrick.df68@email.com>
2025-07-24 13:26:53 -0300

    1.2.35

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1404537245"
+    serviceWorkerVersion: "1019757715"
   }
 });
