ae0daf3def288028ebdf336d127a0383d49331e9
WebPatrickWorks <patrick.df68@email.com>
2025-07-26 23:40:26 -0300

    1.3.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e4fa8e69150d95e5dcc0be3e4bcfbcdb
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "f61203628bea9d7be58c98dcf4153657",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f74f6e5b805c25457c8188cfbe0290e9",
+"flutter_bootstrap.js": "014024730d1420dfba453de919ba85f9",
 "icons/Icon-192.png": "16b0e00778d632dd5f569323d57170d7",
 "icons/Icon-512.png": "a8f4a728ac71dcbfeb4886ec75f5391d",
 "icons/Icon-maskable-192.png": "0a68e83ef9af28bf01419613560fd388",
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e4fa8e69150d95e5dcc0be3e4bcfbcdb
 "icons/logo-transpa.png": "7e36a327dfa4131400fc5a27ac1fc0df",
 "index.html": "586a538cdbb64437bb8ed9db2cc8aafc",
 "/": "586a538cdbb64437bb8ed9db2cc8aafc",
-"main.dart.js": "bfac97e5d9273a6fb1ce735ccbe0bb98",
+"main.dart.js": "4622467d654c4126304ea9937a40cb65",
 "manifest.json": "8efda82224fce8a052c5ff66b5e2f1cd",
 "player.html": "296b86587285f7b2ea65175ad64906d1",
 "version.json": "00e1cb6a49283f19d20524032df5df6f"};
